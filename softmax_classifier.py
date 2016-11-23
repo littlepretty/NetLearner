271f3d01de9d3fa11645b8ce8ddffed07424d49e
Jiaqi Yan@IIT-PC <littlepretty881203@gmail.com>
2016-11-23 11:13:15 -0600

    use encoded data + 1 layer perceptron
    Signed-off-by: Jiaqi Yan@IIT-PC <littlepretty881203@gmail.com>

--- softmax_classifier.py
+++ softmax_classifier.py
@@ -4,25 +4,27 @@ import tensorflow as tf
 from netlearner.utils import accuracy, measure_prediction
 from netlearner.multilayer_perceptron import MultilayerPerceptron
 
-encoded_train_dataset = np.load('encoded_trainset.sae.npy')
+encoded_train_dataset = np.load('encoded_trainset.rbm.npy')
 train_labels = np.load('NSLKDD/train_ref.npy')
 valid_labels = np.load('NSLKDD/valid_ref.npy')
 train_labels = np.concatenate((train_labels, valid_labels), axis=0)
-encoded_test_dataset = np.load('encoded_testset.sae.npy')
+encoded_test_dataset = np.load('encoded_testset.rbm.npy')
 test_labels = np.load('NSLKDD/test_ref.npy')
 
 # use encoded traning and testing data
 num_samples = encoded_train_dataset.shape[0]
 feature_size = encoded_train_dataset.shape[1]
 num_labels = train_labels.shape[1]
-hidden_layer_sizes = []
+hidden_layer_sizes = [1000]
 mp_classifier = MultilayerPerceptron(feature_size,
                                      hidden_layer_sizes, num_labels,
                                      trans_func=tf.nn.sigmoid,
-                                     init_learning_rate=0.9, beta=0.0001)
+                                     init_learning_rate=0.64,
+                                     beta=0.0001)
 batch_size = 240
-num_steps = 80000
-mp_classifier.train(encoded_train_dataset, train_labels, batch_size, num_steps)
+num_steps = 1600000
+mp_classifier.train_with_bias(encoded_train_dataset,
+                              train_labels, batch_size, num_steps)
 test_predict = mp_classifier.make_prediction(encoded_test_dataset)
 test_accuracy = accuracy(test_predict, test_labels)
 measure_prediction(test_predict, test_labels, 'Test')
