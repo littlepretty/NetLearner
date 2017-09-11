0cbd0e9e302d0f22f2a77c1372d5366629359605
Jiaqi Yan@IIT-PC <littlepretty881203@gmail.com>
2017-09-11 10:27:54 -0500

    debugging GAN
    Signed-off-by: Jiaqi Yan@IIT-PC <littlepretty881203@gmail.com>

--- gan_mnist.py
+++ gan_mnist.py
@@ -1,24 +1,26 @@
 import tensorflow as tf
+import numpy as np
 from tensorflow.examples.tutorials.mnist import input_data
 from netlearner.gan import GenerativeAdversarialNets
 from math import ceil
 
 mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
 train_dataset = mnist.train.images
+# normalizing input to [-1, 1] will not work
 train_labels = mnist.train.labels
 
 num_samples, input_dim = train_dataset.shape
 num_labels = train_labels.shape[1]
 noise_dim = 100
-G_hidden_layer = 128
-D_hidden_layer = 128
+G_hidden_layer = 100
+D_hidden_layer = 100
 
 batch_size = 128
-num_epochs = 200
+num_epochs = 120
 init_lr = 0.001
 num_steps = ceil(num_samples / batch_size * num_epochs)
-
 gan = GenerativeAdversarialNets(noise_dim, input_dim,
                                 G_hidden_layer, D_hidden_layer,
                                 trans_func=tf.nn.relu)
 gan.train(batch_size, train_dataset, int(num_steps), init_lr)
+gan.close()
--- netlearner/gan.py
+++ netlearner/gan.py
@@ -20,38 +20,44 @@ class GenerativeAdversarialNets(object):
 
         self.theta_G, self.theta_D = self._create_variables()
 
-        self.learning_rate = tf.placeholder(tf.float32, name='lr')
+        self.lr = tf.placeholder(tf.float32, name='lr')
         self.Z = tf.placeholder(tf.float32, [None, noise_dim], name='noise')
         self.X = tf.placeholder(tf.float32, [None, input_dim], name='input')
 
         self.G_sample = self.generator(self.Z)
         D_real_prob, D_real_logit = self.discriminator(self.X)
         D_fake_prob, D_fake_logit = self.discriminator(self.G_sample)
-        # D_loss_real = -tf.log(D_real_prob)
-        # D_loss_fake = -tf.log(1 - D_fake_prob)
-        D_loss_real = tf.nn.sigmoid_cross_entropy_with_logits(
-            logits=D_real_logit,
-            labels=tf.ones_like(D_real_logit))
-        D_loss_fake = tf.nn.sigmoid_cross_entropy_with_logits(
-            logits=D_fake_logit,
-            labels=tf.zeros_like(D_fake_logit))
-        self.D_loss = tf.reduce_mean(D_loss_real) + tf.reduce_mean(D_loss_fake)
+        D_V_real_neg = -tf.log(D_real_prob)
+        D_V_fake_neg = -tf.log(1.0 - D_fake_prob)
+        # D_V_real_neg = tf.nn.sigmoid_cross_entropy_with_logits(
+            # logits=D_real_logit,
+            # labels=tf.ones_like(D_real_logit))
+        # D_V_fake_neg = tf.nn.sigmoid_cross_entropy_with_logits(
+            # logits=D_fake_logit,
+            # labels=tf.zeros_like(D_fake_logit))
+        self.D_V_real_neg = tf.reduce_mean(D_V_real_neg)
+        self.D_V_fake_neg = tf.reduce_mean(D_V_fake_neg)
+        self.D_V_neg = self.D_V_real_neg + self.D_V_fake_neg
 
-        G_loss = tf.nn.sigmoid_cross_entropy_with_logits(
-            logits=D_fake_logit,
-            labels=tf.ones_like(D_fake_logit))
-        self.G_loss = tf.reduce_mean(G_loss)
-        # self.G_loss = tf.reduce_mean(tf.log(1 - D_fake_prob))
-
-        self.D_solver = optimizer(learning_rate=self.learning_rate).minimize(
-            self.D_loss, var_list=self.theta_D)
-        self.G_solver = optimizer(learning_rate=self.learning_rate).minimize(
-            self.G_loss, var_list=self.theta_G)
+        Gz = self.generator(self.Z)
+        D_fake_prob, D_fake_logit = self.discriminator(Gz)
+        # G_V = tf.nn.sigmoid_cross_entropy_with_logits(
+            # logits=D_fake_logit,
+            # labels=tf.ones_like(D_fake_logit))
+        # self.G_V = tf.reduce_mean(G_V)
+        self.G_V = tf.reduce_mean(tf.log(1.0 - D_fake_prob))
+        """
+        Maximize D_V while minimize G_V
+        """
+        self.D_solver = optimizer(learning_rate=self.lr).minimize(
+            self.D_V_neg, var_list=self.theta_D)
+        self.G_solver = optimizer(learning_rate=self.lr).minimize(
+            self.G_V, var_list=self.theta_G)
 
         time_str = strftime("%b-%d-%Y-%H-%M-%S", localtime())
         self.dirname = name + '/Run-' + time_str
         self.train_writer = tf.summary.FileWriter(self.dirname)
-        self._create_summaries()
+        self._create_summaries(D_real_prob, D_fake_prob)
         self.merged_summary = tf.summary.merge_all()
 
         init = tf.global_variables_initializer()
@@ -62,7 +68,7 @@ class GenerativeAdversarialNets(object):
     def xavier_init(self, size):
         in_dim = size[0]
         xavier_stddev = 1. / tf.sqrt(in_dim / 2.)
-        return tf.random_normal(shape=size, stddev=xavier_stddev)
+        return tf.truncated_normal(shape=size, stddev=xavier_stddev)
 
     def _create_variables(self):
         G_W1 = tf.Variable(self.xavier_init([self.noise_dim,
@@ -82,8 +88,28 @@ class GenerativeAdversarialNets(object):
 
         return theta_G, theta_D
 
-    def _create_summaries(self):
-        pass
+    def _create_summaries(self, D_real, D_fake):
+        tf.summary.scalar('data dimension', self.input_dim)
+        tf.summary.scalar('noise prior dimension', self.noise_dim)
+        tf.summary.scalar('Genearator hidden size', self.G_hidden_layer)
+        tf.summary.scalar('Discriminator hidden size', self.D_hidden_layer)
+        tf.summary.scalar('DV_real', -self.D_V_real_neg)
+        tf.summary.scalar('DV_fake', -self.D_V_fake_neg)
+        tf.summary.scalar('DV', -self.D_V_neg)  # should increase
+        tf.summary.scalar('GV', self.G_V)  # should decrease
+        # tf.summary.histogram('D_real', D_real)
+        # tf.summary.histogram('D_fake', D_fake)
+        tf.summary.histogram('X_fake', self.G_sample)
+        tf.summary.histogram('X_real', self.X)
+        tf.summary.image('Geneated MNIST images',
+                         tf.reshape(self.G_sample, [-1, 28, 28, 1]),
+                         max_outputs=10)
+
+    def make_summary(self, step, X, Z):
+        summaries = self.sess.run(self.merged_summary,
+                                  feed_dict={self.X: X,
+                                             self.Z: Z})
+        self.train_writer.add_summary(summaries, step)
 
     def generator(self, z):
         G_W1, G_b1 = self.theta_G['G_W1'], self.theta_G['G_b1']
@@ -99,12 +125,13 @@ class GenerativeAdversarialNets(object):
         D_W2, D_b2 = self.theta_D['D_W2'], self.theta_D['D_b2']
 
         h1 = self.trans_func(tf.matmul(x, D_W1) + D_b1)
+        # dropout = tf.nn.dropout(h1, keep_prob=1.0)
         D_logit = tf.matmul(h1, D_W2) + D_b2
         D_prob = tf.nn.sigmoid(D_logit)
         return D_prob, D_logit
 
     def sample_noise(self, size):
-        return np.random.uniform(-1.0, 1.0, size=size)
+        return np.random.uniform(0.0, 1.0, size=size)
 
     def plot_samples(self, samples):
         fig = plt.figure(figsize=(4, 4))
@@ -121,44 +148,70 @@ class GenerativeAdversarialNets(object):
 
         return fig
 
-    def train(self, batch_size, train_dataset, num_steps, init_lr):
+    def plot_V(self, D_V, G_V):
+        fig, ax = plt.subplots()
+        ax.plot(xrange(len(D_V)), D_V, 'rs-', label='V(D)')
+        ax.plot(xrange(len(G_V)), G_V, 'bd-', label='V(G)')
+        ax.legend(loc='upper right')
+        plt.grid()
+        plt.savefig('%s/compare_DG.png' % self.dirname, bbox_inches='tight')
+        plt.close(fig)
+
+    def train(self, batch_size, train_dataset, num_steps, init_lr=0.001):
         display_step = num_steps // 10
+        summary_step = num_steps // 100
         perm = np.random.permutation(train_dataset.shape[0])
         X = train_dataset[perm, :]
-        fig_index = 0
-        for step in xrange(num_steps):
-            batch_X = next_batch(X, step, batch_size)
-            batch_Z_D = self.sample_noise([batch_size, self.noise_dim])
-            _, D_loss = self.sess.run([self.D_solver, self.D_loss],
-                                      feed_dict={self.X: batch_X,
-                                                 self.Z: batch_Z_D,
-                                                 self.learning_rate: init_lr})
+        # Use fixed Z to generate samples
+        display_Z = self.sample_noise([16, self.noise_dim])
 
+        fig_index = 0
+        print('Training GAN for %d steps' % num_steps)
+        D_history = []
+        G_history = []
+        for step in xrange(num_steps):
+            for k in range(1):
+                batch_X = next_batch(X, step, batch_size)
+                batch_Z_D = self.sample_noise([batch_size, self.noise_dim])
+                _, D_V_neg = self.sess.run([self.D_solver, self.D_V_neg],
+                                           feed_dict={self.X: batch_X,
+                                                      self.Z: batch_Z_D,
+                                                      self.lr: init_lr})
+            #  finish k steps for training D
             batch_Z_G = self.sample_noise([batch_size, self.noise_dim])
-            _, G_loss = self.sess.run([self.G_solver, self.G_loss],
-                                      feed_dict={self.Z: batch_Z_G,
-                                                 self.learning_rate: init_lr})
+            _, G_V = self.sess.run([self.G_solver, self.G_V],
+                                   feed_dict={self.Z: batch_Z_G,
+                                              self.lr: init_lr})
 
             if step % display_step == 0:
-                print('Batch(%d cases) loss at step %d' % (batch_X.shape[0],
-                                                           step))
-                print('D loss: %.6f, G loss: %.6f' % (D_loss, G_loss))
-                display_Z = self.sample_noise([16, self.noise_dim])
+                print('Batch(%d cases) value function at step %d' %
+                      (batch_X.shape[0], step))
+                print('V(D) = %.6f, V(G) = %.6f' % (-D_V_neg, G_V))
                 samples = self.sess.run(self.G_sample,
                                         feed_dict={self.Z: display_Z})
                 fig = self.plot_samples(samples)
-                plt.savefig('mnish_sample_%d.png' % fig_index,
+                plt.savefig('%s/mnish_sample_%d.png' % (self.dirname,
+                                                        fig_index),
                             bbox_inches='tight')
                 fig_index += 1
                 plt.close(fig)
 
-        print('Finish Training for %d steps' % num_steps)
+            if step % summary_step == 0:
+                D_history.append(-D_V_neg)
+                G_history.append(G_V)
+                self.make_summary(step, batch_X, batch_Z_G)
+
         Z_D = self.sample_noise([X.shape[0], self.noise_dim])
-        D_loss = self.sess.run(self.D_loss,
-                               feed_dict={self.X: X,
-                                          self.Z: Z_D})
+        D_V_neg = self.sess.run(self.D_V_neg,
+                                feed_dict={self.X: X,
+                                           self.Z: Z_D})
 
         Z_G = self.sample_noise([X.shape[0], self.noise_dim])
-        G_loss = self.sess.run(self.G_loss,
-                               feed_dict={self.Z: Z_G})
-        print('D loss: %.6f, G loss: %.6f' % (D_loss, G_loss))
+        G_V = self.sess.run(self.G_V,
+                            feed_dict={self.Z: Z_G})
+        print('Finish training\nV(D) = %.6f, V(G) = %.6f' % (-D_V_neg, G_V))
+        self.make_summary(num_steps, X, Z_G)
+        self.plot_V(D_history, G_history)
+
+    def close(self):
+        self.sess.close()
