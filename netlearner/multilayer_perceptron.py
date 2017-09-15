1bc316d3ab8fbb5442ddee01cce4d687dcc8b34f
Jiaqi.Naruto@IITCPUServer <littlepretty881203@gmail.com>
2017-09-15 15:23:48 -0500

    change tf.float32 to tf.float64
    Signed-off-by: Jiaqi.Naruto@IITCPUServer <littlepretty881203@gmail.com>

--- netlearner/multilayer_perceptron.py
+++ netlearner/multilayer_perceptron.py
@@ -16,30 +16,34 @@ class MultilayerPerceptron(object):
         self.trans_func = trans_func
 
         self.params = self._create_variables()
-        self.x = tf.placeholder(tf.float32, [None, feature_size], name='input')
-        self.t = tf.placeholder(tf.float32, [None, num_labels], name='target')
-        self.keep_prob = tf.placeholder(tf.float32, name='keep_prob')
-        self.learning_rate = tf.placeholder(tf.float32, name='lr')
-        self.train_accuracy_record = tf.placeholder(tf.float32, name='train_accu')
-        self.valid_loss_record = tf.placeholder(tf.float32, name='valid_loss')
-        self.valid_accuracy_record = tf.placeholder(tf.float32, name='valid_accu')
-        self.test_accuracy_record = tf.placeholder(tf.float32, name='test_accu')
+        self.x = tf.placeholder(tf.float64, [None, feature_size], name='input')
+        self.t = tf.placeholder(tf.float64, [None, num_labels], name='target')
+        self.keep_prob = tf.placeholder(tf.float64, name='keep_prob')
+        self.learning_rate = tf.placeholder(tf.float64, name='lr')
+        self.train_accuracy_record = tf.placeholder(tf.float64,
+                                                    name='train_accu')
+        self.valid_loss_record = tf.placeholder(tf.float64, name='valid_loss')
+        self.valid_accuracy_record = tf.placeholder(tf.float64,
+                                                    name='valid_accu')
+        self.test_accuracy_record = tf.placeholder(tf.float64, name='test_accu')
 
         self.final_logits = self._create_forward()
         # self.classify_loss = self._create_classify_loss(class_weights)
-        self.classify_loss = self.weighted_softmax_cross_entropy_with_logits(class_weights)
+        self.classify_loss = \
+            self.weighted_softmax_cross_entropy_with_logits(class_weights)
         self.regterm = self._create_regterm(reg_func, beta)
 
         self.loss = self.classify_loss + self.regterm
-        self.optimizer = optimizer(learning_rate=self.learning_rate).minimize(self.loss)
+        self.optimizer = optimizer(
+            learning_rate=self.learning_rate).minimize(self.loss)
 
         self.predict = tf.nn.softmax(self.final_logits)
 
         time_str = strftime("%b-%d-%Y-%H-%M-%S", localtime())
         self.dirname = name + '/Run-' + time_str
         self.train_writer = tf.summary.FileWriter(self.dirname)
-        # self._create_summaries()
-        # self.merged_summary = tf.summary.merge_all()
+        self._create_summaries()
+        self.merged_summary = tf.summary.merge_all()
 
         init = tf.global_variables_initializer()
         self.sess = tf.Session()
@@ -52,7 +56,7 @@ class MultilayerPerceptron(object):
         for (i, hsize) in enumerate(self.layer_sizes):
             weights['w%d' % i] = tf.Variable(xavier_init(fsize, hsize, 4))
             weights['b%d' % i] = tf.Variable(tf.zeros(shape=[hsize],
-                                                      dtype=tf.float32))
+                                                      dtype=tf.float64))
             fsize = hsize
 
         return weights
@@ -72,10 +76,10 @@ class MultilayerPerceptron(object):
         return next_input
 
     def _create_regterm(self, reg_func, beta):
-        regterm = reg_func([0.0])
+        regterm = reg_func(tf.Variable([0.0], dtype=tf.float64))
         for (i, hsize) in enumerate(self.layer_sizes):
             regterm = tf.add(regterm, reg_func(self.params['w%d' % i]))
-        return tf.multiply(beta, regterm)
+        return beta * regterm
 
     def weighted_softmax_cross_entropy_with_logits(self, class_weights):
         sum_exp_power = tf.reduce_sum(tf.exp(self.final_logits),
@@ -92,33 +96,34 @@ class MultilayerPerceptron(object):
         # prob_dist = tf.nn.softmax(self.final_logits)
         # cross_entropy = tf.multiply(self.t * tf.log(prob_dist), c)
         # classify_loss = tf.reduce_mean(-tf.reduce_sum(cross_entropy,
-                                                      # reduction_indices=[1]))
+        # reduction_indices=[1]))
         # square_root_loss = tf.reduce_mean(
             # tf.multiply(c, tf.pow(self.t - prob_dist, 2)))
-        c = tf.constant(class_weights, dtype=tf.float32)
-        cross_entropy = tf.nn.weighted_cross_entropy_with_logits(logits=self.final_logits,
-                                                                 targets=self.t,
-                                                                 pos_weight=c)
+        c = tf.constant(class_weights, dtype=tf.float64)
+        cross_entropy = \
+            tf.nn.weighted_cross_entropy_with_logits(logits=self.final_logits,
+                                                     targets=self.t,
+                                                     pos_weight=c)
         classify_loss = tf.reduce_mean(cross_entropy)
         return classify_loss
 
     def _create_summaries(self):
         tf.summary.scalar('input dimension', self.feature_size)
         for (layer, size) in enumerate(self.layer_sizes):
-            print("Start visualize features on layer %d" % (layer + 1))
             layer_weight = tf.transpose(self.params['w%d' % layer])
-            x_min = tf.reduce_min(layer_weight)
-            x_max = tf.reduce_max(layer_weight)
-            normalized_layer_weight = tf.div(layer_weight - x_min, x_max - x_min)
-
-            num_neurons = normalized_layer_weight.get_shape()[0].value
-            input_dim = normalized_layer_weight.get_shape()[1].value
-            print("%d * %d matrix" % (num_neurons, input_dim))
-            images = tf.reshape(normalized_layer_weight, [num_neurons, input_dim, 1, 1])
-            tf.summary.image('layer%d' % (layer + 1), images, max_outputs=16)
-
+            # x_min = tf.reduce_min(layer_weight)
+            # x_max = tf.reduce_max(layer_weight)
+            # normalized_layer_weight = tf.div(layer_weight - x_min,
+            # x_max - x_min)
+            # num_neurons = normalized_layer_weight.get_shape()[0].value
+            # input_dim = normalized_layer_weight.get_shape()[1].value
+            # print("%d * %d matrix" % (num_neurons, input_dim))
+            # images = tf.reshape(normalized_layer_weight,
+            # [num_neurons, input_dim, 1, 1])
+            # tf.summary.image('layer%d' % (layer + 1), images, max_outputs=16)
             tf.summary.scalar('layer %d size' % (layer + 1), size)
-            tf.summary.histogram('histogram of layer %d weights' % (layer + 1), layer_weight)
+            tf.summary.histogram('histogram of layer %d weights' % (layer + 1),
+                                 layer_weight)
             # tf.summary.scalar('min weight in layer %d' % (layer + 1), x_min)
             # tf.summary.scalar('max weight in layer %d' % (layer + 1), x_max)
             # mean = tf.reduce_mean(layer_weight)
@@ -127,7 +132,7 @@ class MultilayerPerceptron(object):
             # tf.summary.scalar('stddev in layer %d' % (layer + 1), stddev)
 
         tf.summary.scalar('learning rate', self.learning_rate)
-        tf.summary.scalar('dropout keep probability', self.keep_prob)
+        tf.summary.scalar('keep prob', self.keep_prob)
         # tf.summary.scalar('regularization loss', self.regterm_record)
         tf.summary.scalar('train accuracy', self.train_accuracy_record)
         # tf.summary.scalar('valid loss', self.valid_loss_record)
@@ -135,10 +140,11 @@ class MultilayerPerceptron(object):
         tf.summary.scalar('test accuracy', self.test_accuracy_record)
 
     def fit(self, X, T, lr, prob=0.5):
-        opt, loss, reg = self.sess.run([self.optimizer, self.loss, self.regterm],
-                                       feed_dict={self.x: X, self.t: T,
-                                                  self.keep_prob: prob,
-                                                  self.learning_rate: lr})
+        opt, loss, reg = self.sess.run(
+            [self.optimizer, self.loss, self.regterm],
+            feed_dict={self.x: X, self.t: T,
+                       self.keep_prob: prob,
+                       self.learning_rate: lr})
         return loss, reg
 
     def make_prediction(self, X):
@@ -153,14 +159,16 @@ class MultilayerPerceptron(object):
                                         self.keep_prob: prob})
         return loss
 
-    def make_summary(self, train_accu, valid_loss, valid_accu, test_accu, lr, keep_prob, step):
-        summaries = self.sess.run(self.merged_summary,
-                                  feed_dict={self.keep_prob: keep_prob,
-                                             self.train_accuracy_record: train_accu,
-                                             self.valid_loss_record: valid_loss,
-                                             self.valid_accuracy_record: valid_accu,
-                                             self.test_accuracy_record: test_accu,
-                                             self.learning_rate: lr})
+    def make_summary(self, train_accu, valid_loss, valid_accu,
+                     test_accu, lr, keep_prob, step):
+        summaries = self.sess.run(
+            self.merged_summary,
+            feed_dict={self.keep_prob: keep_prob,
+                       self.train_accuracy_record: train_accu,
+                       self.valid_loss_record: valid_loss,
+                       self.valid_accuracy_record: valid_accu,
+                       self.test_accuracy_record: test_accu,
+                       self.learning_rate: lr})
         self.train_writer.add_summary(summaries, step)
 
     def get_weights(self, name='w0'):
@@ -201,8 +209,10 @@ class MultilayerPerceptron(object):
                 else:
                     train3, label3 = X[3][:, :], Y[3][:, :]
                 train4, label4 = get_batch(X[4], Y[4], step, batch_size)
-                batch_data = np.concatenate((train0, train1, train2, train3, train4), axis=0)
-                batch_labels = np.concatenate((label0, label1, label2, label3, label4), axis=0)
+                batch_data = np.concatenate((train0, train1, train2,
+                                             train3, train4), axis=0)
+                batch_labels = np.concatenate((label0, label1, label2,
+                                               label3, label4), axis=0)
             else:
                 train0, label0 = get_batch(X[0], Y[0], step, batch_size)
                 train1, label1 = get_batch(X[1], Y[1], step, batch_size)
@@ -219,26 +229,31 @@ class MultilayerPerceptron(object):
                 train_predict = self.make_prediction(train_dataset)
                 train_accu = accuracy(train_predict, train_labels)
                 valid_predict = self.make_prediction(valid_dataset)
-                valid_loss, valid_reg = self.sess.run([self.loss, self.regterm],
-                                                      feed_dict={self.x: valid_dataset,
-                                                                 self.t: valid_labels,
-                                                                 self.keep_prob: keep_prob,
-                                                                 self.learning_rate: lr})
+                valid_loss, valid_reg = self.sess.run(
+                    [self.loss, self.regterm],
+                    feed_dict={self.x: valid_dataset,
+                               self.t: valid_labels,
+                               self.keep_prob: keep_prob,
+                               self.learning_rate: lr})
                 valid_accu = accuracy(valid_predict, valid_labels)
                 test_predict = self.make_prediction(test_dataset)
                 test_accu = accuracy(test_predict, test_labels)
-                # self.make_summary(train_accu, valid_loss, valid_accu, test_accu,
-                                  # lr, keep_prob, stpes)
+                self.make_summary(train_accu, valid_loss, valid_accu,
+                                  test_accu, lr, keep_prob, step)
 
             if step != 0 and step % display_step == 0:
                 batch_predict = self.make_prediction(batch_data)
                 train_predict = self.make_prediction(train_dataset)
                 valid_predict = self.make_prediction(valid_dataset)
-                print("Minibatch(%d cases) loss at step %d: %.6f(regterm=%.4f, lr=%.6f)"
-                      % (batch_labels.shape[0], step, loss, reg, lr))
-                print("Minibatch train accuracy: %f%%" % accuracy(batch_predict, batch_labels))
-                print("Trainset accuracy: %f%%" % accuracy(train_predict, train_labels))
-                print("Validation accuracy: %f%%" % accuracy(valid_predict, valid_labels))
+                print("Minibatch(%d cases) loss at step %d: %.6f\
+                      (regterm=%.4f, lr=%.6f)" % (batch_labels.shape[0],
+                                                  step, loss, reg, lr))
+                print("Minibatch train accuracy: %f%%" %
+                      accuracy(batch_predict, batch_labels))
+                print("Trainset accuracy: %f%%" %
+                      accuracy(train_predict, train_labels))
+                print("Validation accuracy: %f%%" %
+                      accuracy(valid_predict, valid_labels))
 
         print('Multilayer Perceptron trained')
 
@@ -256,5 +271,5 @@ class MultilayerPerceptron(object):
         test_accu = accuracy(test_predict, test_labels)
         measure_prediction(test_predict, test_labels, self.dirname, 'Test')
 
-        # self.make_summary(train_accu, valid_loss, valid_accu, test_accu,
-                          # lr, keep_prob, num_stpes)
+        self.make_summary(train_accu, valid_loss, valid_accu, test_accu,
+                          lr, keep_prob, num_steps)
