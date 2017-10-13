fb866abef0eb70381d8526545835fc1a98de1667
Jiaqi Yan@IIT-PC <littlepretty881203@gmail.com>
2017-10-13 15:34:40 -0500

    visualizing code
    Signed-off-by: Jiaqi Yan@IIT-PC <littlepretty881203@gmail.com>

--- visualize_unsw_importance.py
+++ visualize_unsw_importance.py
@@ -8,17 +8,15 @@ from netlearner.utils import quantile_transform
 
 generate_dataset(one_hot_encode=False)
 raw = np.load('UNSW/train_dataset.npy')
-minmax, _, _ = min_max_scale(raw, None, None)
 train_labels = np.load('UNSW/train_labels.npy')
 y = np.argmax(train_labels, 1)
 
 plot_feature_importance(raw, y, 'UNSW', 'raw')
-
 columns = np.array(range(1, 6) + range(8, 16) + range(17, 19) +
                    range(23, 25) + [26])
+minmax, _, _ = min_max_scale(raw, None, None)
 augment = augment_quantiled(raw, None, None, columns)
 replace = quantile_transform(minmax, None, None, columns)
-
 plot_pca_components(minmax, y, 'UNSW', 'raw')
 plot_pca_components(augment, y, 'UNSW', 'augment_quantile')
 plot_pca_components(replace, y, 'UNSW', 'quantile_transform')
