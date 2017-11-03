fd8307f51076e81bad1a2fc40d794b12cb6ff797
Jiaqi.Naruto@IITCPUServer <littlepretty881203@gmail.com>
2017-11-03 17:12:05 -0500

    runnable keras merge model
    Signed-off-by: Jiaqi.Naruto@IITCPUServer <littlepretty881203@gmail.com>

--- kmerger.py
+++ kmerger.py
@@ -4,7 +4,7 @@ from keras.layers import Embedding, BatchNormalization
 
 from preprocess.unsw import get_feature_names, discovery_feature_volcabulary
 from preprocess.unsw import discovery_integer_map, discovery_continuous_map
-from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import LabelEncoder, MinMaxScaler
 
 import pandas as pd
 import numpy as np
@@ -22,13 +22,15 @@ df = pd.read_csv(dataset_names[0],
 X = df.drop('attack_cat', axis=1)
 Y = df['label'].astype(int)
 train_dict = dict()
+merged_dim = 0
+merged_inputs = []
 
 # Define embedding layers/inputs
 embeddings = []
-embedding_inputs = []
+
 for (name, values) in symbolic_features.items():
     column = Input(shape=(1, ), name=name)
-    embedding_inputs.append(column)
+    merged_inputs.append(column)
     raw_data = X[name].as_matrix()
     le = LabelEncoder()
     le.fit(raw_data)
@@ -36,45 +38,63 @@ for (name, values) in symbolic_features.items():
 
     dim_V = len(values)
     dim_E = int(min(7, np.ceil(np.log2(dim_V))))
-    print('Dimension of E and V', dim_E, dim_V)
+    print('Dimension of %s E=%s and V=%s' % (name, dim_E, dim_V))
     temp = Embedding(output_dim=dim_E, input_dim=dim_V,
                      input_length=1, name='embed_%s' % name)(column)
     temp = Flatten(name='flat_%s' % name)(temp)
     embeddings.append(temp)
+    merged_dim += dim_E
 
+large_inputs = []
 for (name, values) in integer_features.items():
     column = Input(shape=(1, ), name=name)
-    embedding_inputs.append(column)
-    train_dict[name] = X[name].astype('int64').as_matrix()
-
+    merged_inputs.append(column)
+    raw_data = X[name].astype('int64').as_matrix()
     dim_V = int(values['max'] - values['min'] + 1)
-    dim_E = int(min(5, np.ceil(np.log2(dim_V))))
-    print('Dimension of E and V', dim_E, dim_V)
-    temp = Embedding(output_dim=dim_E, input_dim=dim_V,
-                     input_length=1, name='embed_%s' % name)(column)
-    temp = Flatten(name='flat_%s' % name)(temp)
-    embeddings.append(temp)
+
+    if dim_V < 8096:
+        train_dict[name] = raw_data - values['min']
+        dim_E = int(min(5, np.ceil(np.log2(dim_V))))
+        print('Dimension of %s E=%s and V=%s' % (name, dim_E, dim_V))
+        temp = Embedding(output_dim=dim_E, input_dim=dim_V,
+                         input_length=1, name='embed_%s' % name)(column)
+        temp = Flatten(name='flat_%s' % name)(temp)
+        embeddings.append(temp)
+        merged_dim += dim_E
+    else:
+        large_inputs.append(column)
+        print('Large feature %s is treated as continuous' % name)
+        mm = MinMaxScaler()
+        raw_data = raw_data.reshape((len(raw_data), 1))
+        mm.fit(raw_data)
+        train_dict[name] = mm.transform(raw_data)
+        merged_dim += 1
+
 
 continuous_features = discovery_continuous_map(feature_file, dataset_names)
-print('symbolic %s' % symbolic_features)
-print('integer %s' % integer_features)
-print('continuous %s' % continuous_features)
-
 continuous_inputs = Input(shape=(len(continuous_features), ),
                           name='continuous')
-train_dict['continuous'] = X[continuous_features.keys()].as_matrix()
+merged_inputs.append(continuous_inputs)
+raw_data = X[continuous_features.keys()].as_matrix()
+mm = MinMaxScaler()
+mm.fit(raw_data)
+train_dict['continuous'] = mm.transform(raw_data)
+merged_dim += len(continuous_features)
 
-merge = concatenate(embeddings + [continuous_inputs], name='merged_input')
-h1 = Dense(256, activation='relu', name='hidden1')(merge)
+print('merge input_dim for this dataset = %s' % merged_dim)
+merge = concatenate(embeddings + large_inputs + [continuous_inputs],
+                    name='merge_features')
+h1 = Dense(400, activation='relu', name='hidden1')(merge)
 # h2 = Dense(320, activation='relu', name='hidden2')(h1)
 encode = BatchNormalization(name='unified_x')(h1)
 
-h4 = Dense(400, activation='sigmoid', name='hidden_all')(encode)
+h4 = Dense(1024, activation='sigmoid', name='hidden_all')(encode)
 sm = Dense(1, activation='softmax', name='output')(h4)
 
 print('input/output dict %s' % train_dict)
-model = Model(inputs=embedding_inputs + [continuous_inputs], outputs=sm)
-model.compile(optimizer='rmsprop', loss='binary_crossentropy')
+model = Model(inputs=merged_inputs, outputs=sm)
+model.compile(optimizer='adam', loss='binary_crossentropy',
+              metrics=['accuracy'])
 model.summary()
 model.fit(train_dict, {'output': Y.as_matrix()},
           epochs=1, batch_size=40)
