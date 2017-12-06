6c01f5560cd5ffc12edf99d2293c98a10c5a361f
ngetty <ngetty@hawk.iit.edu>
2017-12-06 13:51:40 -0600

    scores now logged

--- lgbm_unified.py
+++ lgbm_unified.py
@@ -1,12 +1,12 @@
 from keras.models import Model
-from keras.layers import Dense, Input
+from keras.layers import Dense, Input, merge
 # from keras.layers import Dropout
 from keras import regularizers
 from keras.layers import BatchNormalization
 from keras.callbacks import CSVLogger
 # from keras import initializers
 from lightgbm import LGBMClassifier
-
+import pickle
 import tensorflow as tf
 from keras.backend import tensorflow_backend as K
 
@@ -85,36 +85,63 @@ def multimodal_autoencoder(unsw_dim, nsl_dim, H1, U, sparse=0.00):
     return model_unsw, model_nsl, encoder_unsw, encoder_nsl
 
 
+def build_attention_model(input_dim, nb_classes):
+    inputs = Input(shape=(input_dim[0],))
+
+    # ATTENTION PART STARTS HERE
+    attention_probs = Dense(input_dim[0], activation='softmax', name='attention_vec')(inputs)
+    attention_mul = merge([inputs, attention_probs], output_shape=nb_classes, name='attention_mul', mode='mul')
+    # ATTENTION PART FINISHES HERE
+
+    attention_mul = Dense(64)(attention_mul)
+    output = Dense(units=nb_classes, activation='sigmoid')(attention_mul)
+    model = Model(input=[inputs], output=output)
+
+    return model
+
+
 def supervised_shared(unsw_dict, nsl_dict, H1, U, num_epochs, batch_size, beta):
-    logger.info('Using Shared AE with Linear Classifier')
-    X_unsw = unsw_dict['X']
-    X_unsw_test = unsw_dict['X_test']
-    y_unsw = unsw_dict['y']
-    y_unsw_test = unsw_dict['y_test']
+    load = False
+    if load:
+        with open(r"SharedAutoEncoder/datasets.p", "rb") as i:
+            EX_unsw, EX_unsw_test, EX_nsl, EX_nsl_test = pickle.load(i)
+    else:
+        logger.info('Using Shared AE with Linear Classifier')
+        X_unsw = unsw_dict['X']
+        X_unsw_test = unsw_dict['X_test']
+        y_unsw = unsw_dict['y']
+        y_unsw_test = unsw_dict['y_test']
 
-    X_nsl = nsl_dict['X']
-    X_nsl_test = nsl_dict['X_test']
-    y_nsl = nsl_dict['y']
-    y_nsl_test = nsl_dict['y_test']
-    unsw_dim = X_unsw.shape[1]
-    nsl_dim = X_nsl.shape[1]
+        X_nsl = nsl_dict['X']
+        X_nsl_test = nsl_dict['X_test']
+        y_nsl = nsl_dict['y']
+        y_nsl_test = nsl_dict['y_test']
 
-    model_unsw, model_nsl, encoder_unsw, encoder_nsl = multimodal_autoencoder(
-        unsw_dim, nsl_dim, H1, U)
-    for _ in range(num_epochs * 2):
-        model_unsw.fit(X_unsw, X_unsw, epochs=2, batch_size=batch_size)
-        model_nsl.fit(X_nsl, X_nsl, epochs=2, batch_size=batch_size)
+        unsw_dim = X_unsw.shape[1]
+        nsl_dim = X_nsl.shape[1]
 
-    # Get the shared representation of both datasets
-    EX_unsw = encoder_unsw.predict(X_unsw)
-    EX_unsw_test = encoder_unsw.predict(X_unsw_test)
+        model_unsw, model_nsl, encoder_unsw, encoder_nsl = multimodal_autoencoder(
+            unsw_dim, nsl_dim, H1, U)
+        for x in range(num_epochs):
+            print("Epoch:",x)
+            model_unsw.fit(X_unsw, X_unsw, epochs=2, batch_size=batch_size)
+            model_nsl.fit(X_nsl, X_nsl, epochs=2, batch_size=batch_size)
 
-    EX_nsl = encoder_nsl.predict(X_nsl)
-    EX_nsl_test = encoder_nsl.predict(X_nsl_test)
+        # Get the shared representation of both datasets
+        EX_unsw = encoder_unsw.predict(X_unsw)
+        EX_unsw_test = encoder_unsw.predict(X_unsw_test)
 
-    # Get accu5(unsw) and accu5(nsl)
-    EX_concat = np.concatenate((EX_unsw, EX_nsl), axis=0)
-    y_concat = np.concatenate((y_unsw, y_nsl), axis=0)
+        EX_nsl = encoder_nsl.predict(X_nsl)
+        EX_nsl_test = encoder_nsl.predict(X_nsl_test)
+
+        with open(r"SharedAutoEncoder/datasets.p", "wb") as o:
+            pickle.dump((EX_unsw, EX_unsw_test, EX_nsl, EX_nsl_test), o)
+
+        # Get accu5(unsw) and accu5(nsl)
+        #EX_concat = np.concatenate((EX_unsw, EX_nsl), axis=0)
+        #y_concat = np.concatenate((y_unsw, y_nsl), axis=0)
+
+    #model = build_attention_model(EX_unsw.shape[1], 2)
 
     model = LGBMClassifier(n_jobs=8
                            , max_depth=11
@@ -122,29 +149,31 @@ def supervised_shared(unsw_dict, nsl_dict, H1, U, num_epochs, batch_size, beta):
                            , learning_rate=0.1
                            , n_estimators=500
                            # ,max_bin=15
-                           , colsample_bytree=0.8
-                           , subsample=0.8
-                           , min_child_weight=6
+                           #, colsample_bytree=0.8
+                           #, subsample=0.8
+                           #, min_child_weight=6
                            )
 
-    model.fit(EX_concat, y_concat)
-    # pred = model.predict(test_dataset)
-    logger.info("Training lgbm model on unified representation")
-    
-    print("Shared model UNSW train acc:", model.score(EX_unsw, y_unsw))
-    print("Shared model UNSW test acc:", model.score(EX_unsw_test, y_unsw_test))
-    print("Shared model NSL train acc:", model.score(EX_nsl, y_nsl))
-    print("Shared model NSL test acc:", model.score(EX_nsl_test, y_nsl_test))
 
-    logger.info("Shared model UNSW train acc:", model.score(EX_unsw, y_unsw))
-    logger.info("Shared model UNSW test acc:", model.score(EX_unsw_test, y_unsw_test))
-    logger.info("Shared model NSL train acc:", model.score(EX_nsl, y_nsl))
-    logger.info("Shared model NSL test acc:", model.score(EX_nsl_test, y_nsl_test))
-    
+    #model.fit(EX_concat, y_concat[:,0])
+    logger.info("Training lgbm model on NSL unified representation")
+    model.fit(EX_nsl, y_nsl[:, 0], early_stopping_rounds=3, eval_set=(EX_nsl_test, y_nsl_test[:, 0]), verbose=False)
+    logger.info("Shared model NSL train acc:\t%.6f" % model.score(EX_nsl, y_nsl[:, 0]))
+    logger.info("Shared model NSL test acc:\t%.6f" % model.score(EX_nsl_test, y_nsl_test[:, 0]))
 
+    logger.info("Training lgbm model on UNSW unified representation")
+    model.fit(EX_unsw, y_unsw[:, 0], early_stopping_rounds=3, eval_set=(EX_unsw_test, y_unsw_test[:, 0]), verbose=False)
+    #model.fit(EX_nsl, y_nsl[:, 0], early_stopping_rounds=3, eval_set=(EX_nsl_test, y_nsl_test[:, 0]), verbose=False)
+
+
+    logger.info("Shared model UNSW train acc:\t%.6f" % model.score(EX_unsw, y_unsw[:,0]))
+    logger.info("Shared model UNSW test acc:\t%.6f" % model.score(EX_unsw_test, y_unsw_test[:,0]))
+    logger.info("Shared model NSL train acc:\t%.6f" % model.score(EX_nsl, y_nsl[:,0]))
+    logger.info("Shared model NSL test acc:\t%.6f" % model.score(EX_nsl_test, y_nsl_test[:,0]))
+    
 
 def run_master(unsw_dict, nsl_dict, H1, U):
-    num_epochs = 20
+    num_epochs = 1
     batch_size = 64
     beta = 0.01
     supervised_shared(unsw_dict, nsl_dict, H1, U, num_epochs, batch_size, beta)
@@ -163,8 +192,8 @@ if __name__ == '__main__':
     nsl_dict = process_nsl()
 
     # layer_sizes = [128, 240, 320, 400]
-    layer_sizes = [400, 512]
-    num_runs = 10
+    layer_sizes = [512]
+    num_runs = 1
     for H1 in layer_sizes:
         logger.info('********************************************************')
         logger.info('**** Start %d runs with various layer config %d *******'
