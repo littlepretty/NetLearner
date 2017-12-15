d242576c58941e29a886fdc415bbf177ab4b80bf
Jiaqi.Naruto@IITCPUServer <littlepretty881203@gmail.com>
2017-12-15 14:07:22 -0600

    put multimodal AE back
    Signed-off-by: Jiaqi.Naruto@IITCPUServer <littlepretty881203@gmail.com>

--- multimodal_autoencoder.py
+++ multimodal_autoencoder.py
@@ -2,7 +2,7 @@ from keras.models import Model
 from keras.layers import Dense, Input
 # from keras.layers import Dropout
 from keras import regularizers
-from keras.layers import BatchNormalization
+# from keras.layers import BatchNormalization
 from keras.callbacks import CSVLogger
 # from keras import initializers
 
@@ -26,12 +26,12 @@ def multicore_session():
     K.set_session(session)
 
 
-def process_unsw():
-    unsw.generate_dataset(True)
-    raw_X_train = np.load('UNSW/train_dataset.npy')
-    y_train = np.load('UNSW/train_labels.npy')
-    raw_X_test = np.load('UNSW/test_dataset.npy')
-    y_test = np.load('UNSW/test_labels.npy')
+def process_unsw(root='/home/naruto/NetLearner'):
+    unsw.generate_dataset(False)
+    raw_X_train = np.load('%s/UNSW/train_dataset.npy' % root)
+    y_train = np.load('%s/UNSW/train_labels.npy' % root)
+    raw_X_test = np.load('%s/UNSW/test_dataset.npy' % root)
+    y_test = np.load('%s/UNSW/test_labels.npy' % root)
     [X_train, _, X_test] = min_max_scale(raw_X_train, None, raw_X_test)
     permutate_dataset(X_train, y_train)
     permutate_dataset(X_test, y_test)
@@ -41,12 +41,12 @@ def process_unsw():
     return {'X': X_train, 'y': y_train, 'X_test': X_test, 'y_test': y_test}
 
 
-def process_nsl():
-    nslkdd.generate_datasets(binary_label=True)
-    raw_X_train = np.load('NSLKDD/train_dataset.npy')
-    y_train = np.load('NSLKDD/train_labels.npy')
-    raw_X_test = np.load('NSLKDD/test_dataset.npy')
-    y_test = np.load('NSLKDD/test_labels.npy')
+def process_nsl(root='/home/naruto/NetLearner'):
+    nslkdd.generate_datasets(binary_label=True, one_hot_encoding=False)
+    raw_X_train = np.load('%s/NSLKDD/train_dataset.npy' % root)
+    y_train = np.load('%s/NSLKDD/train_labels.npy' % root)
+    raw_X_test = np.load('%s/NSLKDD/test_dataset.npy' % root)
+    y_test = np.load('%s/NSLKDD/test_labels.npy' % root)
     [X_train, _, X_test] = min_max_scale(raw_X_train, None, raw_X_test)
     permutate_dataset(X_train, y_train)
     permutate_dataset(X_test, y_test)
@@ -60,20 +60,20 @@ def single_encoder(feature_dim, H1, U):
     input_layer = Input(shape=(feature_dim, ), name='unsw')
 
     h1 = Dense(H1, activation='relu', name='h1')(input_layer)
-    bn1 = BatchNormalization(name='bn1')(h1)
+    # bn1 = BatchNormalization(name='bn1')(h1)
 
-    encoding = Dense(U, activation='relu', name='encoding')(bn1)
-    bn2 = BatchNormalization(name='bn2')(encoding)
+    encoding = Dense(U, activation='relu', name='encoding')(h1)
+    # bn2 = BatchNormalization(name='bn2')(encoding)
 
-    h3 = Dense(H1, activation='relu', name='h3')(bn2)
-    bn3 = BatchNormalization(name='bn3')(h3)
+    h3 = Dense(H1, activation='relu', name='h3')(encoding)
+    # bn3 = BatchNormalization(name='bn3')(h3)
 
-    h4 = Dense(feature_dim, activation='relu', name='h4')(bn3)
+    h4 = Dense(feature_dim, activation='sigmoid', name='h4')(h3)
 
     model = Model(inputs=input_layer, outputs=h4)
     model.compile(optimizer='adam', loss='binary_crossentropy')
 
-    encoder = Model(inputs=input_layer, outputs=bn2)  # or bn2
+    encoder = Model(inputs=input_layer, outputs=encoding)  # or bn2
     return model, encoder
 
 
@@ -85,32 +85,32 @@ def multimodal_autoencoder(unsw_dim, nsl_dim, H1, U, sparse=0.00):
                     activity_regularizer=regularizers.l1(sparse))(unsw)
     h1_nsl = Dense(H1, activation='relu', name='h1_nsl',
                    activity_regularizer=regularizers.l1(sparse))(nsl)
-    h1_unsw = BatchNormalization(name='bn1_unsw')(h1_unsw)
-    h1_nsl = BatchNormalization(name='bn1_nsl')(h1_nsl)
+    # h1_unsw = BatchNormalization(name='bn1_unsw')(h1_unsw)
+    # h1_nsl = BatchNormalization(name='bn1_nsl')(h1_nsl)
 
     shared_ae = Dense(U, activation='relu', name='shared',
                       activity_regularizer=regularizers.l1(sparse))
     shared_unsw = shared_ae(h1_unsw)
     shared_nsl = shared_ae(h1_nsl)
-    bns_unsw = BatchNormalization(name='bn2_unsw')(shared_unsw)
-    bns_nsl = BatchNormalization(name='bn2_nsl')(shared_nsl)
+    # bns_unsw = BatchNormalization(name='bn2_unsw')(shared_unsw)
+    # bns_nsl = BatchNormalization(name='bn2_nsl')(shared_nsl)
 
-    h3_unsw = Dense(H1, activation='relu', name='h3_unsw')(bns_unsw)
-    h3_nsl = Dense(H1, activation='relu', name='h3_nsl')(bns_nsl)
-    h3_unsw = BatchNormalization(name='bn3_unsw')(h3_unsw)
-    h3_nsl = BatchNormalization(name='bn3_nsl')(h3_nsl)
+    h3_unsw = Dense(H1, activation='relu', name='h3_unsw')(shared_unsw)
+    h3_nsl = Dense(H1, activation='relu', name='h3_nsl')(shared_nsl)
+    # h3_unsw = BatchNormalization(name='bn3_unsw')(h3_unsw)
+    # h3_nsl = BatchNormalization(name='bn3_nsl')(h3_nsl)
 
-    h4_unsw = Dense(unsw_dim, activation='relu', name='h4_unsw')(h3_unsw)
-    h4_nsl = Dense(nsl_dim, activation='relu', name='h4_nsl')(h3_nsl)
+    h4_unsw = Dense(unsw_dim, activation='sigmoid', name='h4_unsw')(h3_unsw)
+    h4_nsl = Dense(nsl_dim, activation='sigmoid', name='h4_nsl')(h3_nsl)
 
     model_unsw = Model(inputs=unsw, outputs=h4_unsw)
-    model_unsw.compile(optimizer='adadelta', loss='binary_crossentropy')
+    model_unsw.compile(optimizer='adam', loss='binary_crossentropy')
     model_unsw.summary()
     model_nsl = Model(inputs=nsl, outputs=h4_nsl)
-    model_nsl.compile(optimizer='adadelta', loss='binary_crossentropy')
+    model_nsl.compile(optimizer='adam', loss='binary_crossentropy')
     model_nsl.summary()
-    encoder_unsw = Model(inputs=unsw, outputs=bns_unsw)
-    encoder_nsl = Model(inputs=nsl, outputs=bns_nsl)
+    encoder_unsw = Model(inputs=unsw, outputs=shared_unsw)
+    encoder_nsl = Model(inputs=nsl, outputs=shared_nsl)
 
     return model_unsw, model_nsl, encoder_unsw, encoder_nsl
 
@@ -129,10 +129,9 @@ def linear_model(feature_dim, reg_beta=0.00):
 def train_single_encoder(X, X_test, H1, U, num_epochs, batch_size, name):
     feature_dim = X.shape[1]
     model, encoder = single_encoder(feature_dim, H1, U)
-    csv_logger = CSVLogger('SharedAutoEncoder/ae_%s.history' % name,
-                           append=True)
+    csv_logger = CSVLogger(root_dir + '/ae_%s.history' % name, append=True)
     model.fit(X, X, epochs=num_epochs, batch_size=batch_size,
-              callbacks=[csv_logger])
+              callbacks=[csv_logger], verbose=1)
     EX = encoder.predict(X)
     EX_test = encoder.predict(X_test)
 
@@ -142,7 +141,7 @@ def train_single_encoder(X, X_test, H1, U, num_epochs, batch_size, name):
 def train_linear_model(X, y, X_test, y_test, num_epochs, batch_size, beta):
     feature_dim = X.shape[1]
     classifier = linear_model(feature_dim, beta)
-    classifier.fit(X, y, batch_size=batch_size, epochs=10)  # fixed for LM
+    classifier.fit(X, y, batch_size=batch_size, epochs=8, verbose=1)
     scores = classifier.evaluate(X_test, y_test, batch_size=X_test.shape[0])
     return scores, classifier
 
@@ -178,16 +177,10 @@ def supervised_single(unsw_dict, nsl_dict, H1, U, num_epochs, batch_size, beta):
     logger.info('Trained on concat unshared-encoding, NSL accu6\t%.6f'
                 % scores6N[1])
     # Get accu3
-    EX_unsw, EX_unsw_test = train_single_encoder(X_unsw, X_unsw_test,
-                                                 H1, U, num_epochs,
-                                                 batch_size, 'unsw')
     scores3, _ = train_linear_model(EX_unsw, y_unsw, EX_unsw_test, y_unsw_test,
                                     num_epochs, batch_size, beta)
     logger.info('Trained on single UNSW-encoding, accu3\t%.6f' % scores3[1])
     # Get accu1
-    EX_nsl, EX_nsl_test = train_single_encoder(X_nsl, X_nsl_test,
-                                               H1, U, num_epochs,
-                                               batch_size, 'nsl')
     scores1, _ = train_linear_model(EX_nsl, y_nsl, EX_nsl_test, y_nsl_test,
                                     num_epochs, batch_size, beta)
     logger.info('Trained on single NSL-encoding, accu1\t%.6f' % scores1[1])
@@ -211,9 +204,11 @@ def supervised_shared(unsw_dict, nsl_dict, H1, U, num_epochs, batch_size, beta):
 
     model_unsw, model_nsl, encoder_unsw, encoder_nsl = multimodal_autoencoder(
         unsw_dim, nsl_dim, H1, U)
-    for _ in range(num_epochs * 2):
-        model_unsw.fit(X_unsw, X_unsw, epochs=2, batch_size=batch_size)
-        model_nsl.fit(X_nsl, X_nsl, epochs=2, batch_size=batch_size)
+    for _ in range(num_epochs):
+        model_unsw.fit(X_unsw, X_unsw, epochs=2,
+                       batch_size=batch_size, verbose=1)
+        model_nsl.fit(X_nsl, X_nsl, epochs=2,
+                      batch_size=batch_size, verbose=1)
 
     # Get the shared representation of both datasets
     EX_unsw = encoder_unsw.predict(X_unsw)
@@ -247,20 +242,24 @@ def supervised_shared(unsw_dict, nsl_dict, H1, U, num_epochs, batch_size, beta):
 
 
 def run_master(unsw_dict, nsl_dict, H1, U):
-    num_epochs = 20
+    num_epochs = 6
     batch_size = 64
     beta = 0.01
     multicore_session()
     logger.info('Network config: %s %s %s %s for %d train epochs and %d batch'
                 % (H1, U, U, H1, num_epochs, batch_size))
-    supervised_single(unsw_dict, nsl_dict, H1, U, num_epochs, batch_size, beta)
-    supervised_shared(unsw_dict, nsl_dict, H1, U, num_epochs, batch_size, beta)
+    part1 = supervised_single(unsw_dict, nsl_dict, H1, U,
+                              num_epochs, batch_size, beta)
+    part2 = supervised_shared(unsw_dict, nsl_dict, H1, U,
+                              num_epochs, batch_size, beta)
+    return dict(part1, **part2)
 
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
-    logger = logging.getLogger('SharedAE')
-    hdlr = logging.FileHandler('SharedAutoEncoder/accuracy.log')
+    logger = logging.getLogger('SharedAEX2')
+    root_dir = 'SharedAutoEncoder'
+    hdlr = logging.FileHandler(root_dir + '/accuracy.log')
     formatter = logging.Formatter('%(asctime)s %(message)s')
     hdlr.setFormatter(formatter)
     logger.addHandler(hdlr)
@@ -270,14 +269,18 @@ if __name__ == '__main__':
     nsl_dict = process_nsl()
 
     # layer_sizes = [128, 240, 320, 400]
-    layer_sizes = [400, 512]
-    num_runs = 10
+    layer_sizes = [400]
+    num_runs = 1
+    mult = 2
+    results = []
     for H1 in layer_sizes:
-        logger.info('********************************************************')
-        logger.info('**** Start %d runs with various layer config %d *******'
+        logger.info('***************************************************')
+        logger.info('**** Start %d runs with layer config %d *******'
                     % (num_runs, H1))
-        logger.info('********************************************************')
+        logger.info('***************************************************')
         for i in range(num_runs):
             logger.info('*** Run index %d ***' % i)
-            run_master(unsw_dict, nsl_dict, H1, H1 * 2)
+            results.append(run_master(unsw_dict, nsl_dict, H1, H1 * mult))
             tf.reset_default_graph()
+        np.save(root_dir + '/result_%dX%d_run%d.npy' % (H1, mult, num_runs),
+                results)
