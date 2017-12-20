22ca0ac16a401038d15f55b2954f0aa2ecb94097
Jiaqi.Naruto@IITCPUServer <littlepretty881203@gmail.com>
2017-12-20 16:50:40 -0600

    Rewrite shared layer to run auto
    Signed-off-by: Jiaqi.Naruto@IITCPUServer <littlepretty881203@gmail.com>

--- shared_layer.py
+++ shared_layer.py
@@ -1,17 +1,16 @@
 from keras.models import Model
 from keras.layers import Dense, Input, concatenate, Flatten, Dropout
-from keras import regularizers
+# from keras import regularizers
 from keras.layers import Embedding, BatchNormalization
 from keras.callbacks import CSVLogger
 
 from preprocess import unsw, nslkdd
-from netlearner.utils import permutate_dataset
 from sklearn.preprocessing import LabelEncoder, MinMaxScaler
-
+from pprint import pprint
+import pickle
 import pandas as pd
 import numpy as np
 import logging
-from res50_nt import Res50NT
 
 
 def get_dataset(dataset_filename, headers, dataset_name):
@@ -183,78 +182,109 @@ def get_nsl_data():
     return merge, merged_inputs, train_dict, test_dict, y, test_y
 
 
-def intermed_model(input, reg_beta=0.001):
-    h1 = Dense(800, activation='relu', name='h1',
-               kernel_regularizer=regularizers.l2(reg_beta))(input)
-    dropout = Dropout(0.2)(h1)
-    bn = BatchNormalization(name='bn_1')(dropout)
-    h2 = Dense(400, activation='sigmoid', name='h2',
-               kernel_regularizer=regularizers.l2(reg_beta))(bn)
-    return h2
+def shared_models(unsw, nsl, unsw_inputs, nsl_inputs, hidden, dropprob=0.2):
+    h1_unsw = Dense(hidden[0], activation='relu', name='h1_unsw')(unsw)
+    h1_nsl = Dense(hidden[0], activation='relu', name='h1_nsl')(nsl)
+    h1_unsw = Dropout(dropprob)(h1_unsw)
+    h1_nsl = Dropout(dropprob)(h1_nsl)
 
+    shared_h2 = Dense(hidden[1], activation='relu', name='shared_layer')
+    h2_unsw = shared_h2(h1_unsw)
+    h2_nsl = shared_h2(h1_nsl)
 
-def master_model(unsw_merged, nsl_merged, unsw_inputs, nsl_inputs, reg_beta=0.001):
-    unsw_h2 = intermed_model(unsw_merged)
-    nsl_h2 = intermed_model(nsl_merged)
+    bn_unsw = BatchNormalization(name='bn_unsw')(h2_unsw)
+    bn_nsl = BatchNormalization(name='bn_nsl')(h2_nsl)
 
-    sm = Dense(2, activation='softmax', name='output')
+    shared_h3 = Dense(hidden[2], activation='sigmoid', name='h3_unsw')
+    h3_unsw = shared_h3(bn_unsw)
+    h3_nsl = shared_h3(bn_nsl)
 
-    unsw_model = Model(inputs=unsw_inputs, outputs=sm(unsw_h2))
+    shared_sm = Dense(2, activation='softmax', name='output')
+    output_unsw = shared_sm(h3_unsw)
+    output_nsl = shared_sm(h3_nsl)
+
+    unsw_model = Model(inputs=unsw_inputs, outputs=output_unsw)
     unsw_model.compile(optimizer='adam', loss='binary_crossentropy',
-                  metrics=['accuracy'])
+                       metrics=['accuracy'])
 
-    nsl_model = Model(inputs=nsl_inputs, output=sm(nsl_h2))
+    nsl_model = Model(inputs=nsl_inputs, output=output_nsl)
     nsl_model.compile(optimizer='adam', loss='binary_crossentropy',
-                  metrics=['accuracy'])
+                      metrics=['accuracy'])
 
     unsw_model.summary()
     nsl_model.summary()
     return unsw_model, nsl_model
 
 
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger('modality_nets')
-hdlr = logging.FileHandler('ModalityNets/accuracy_comp.log')
-formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
-hdlr.setFormatter(formatter)
-logger.addHandler(hdlr)
-logger.setLevel(logging.INFO)
+def run_main(hidden):
+    num_epochs = 20
+    batch_size = 100
+    m1, m2 = shared_models(unsw_tens, nsl_tens, unsw_inputs, nsl_inputs, hidden)
+    csv_logger = CSVLogger(root + 'history.log', append=True)
+    unsw_loss, nsl_loss = [], []
+    for _ in range(num_epochs):
+        m1.fit(X_unsw, y_unsw, batch_size, epochs=1, callbacks=[csv_logger])
+        score = m1.evaluate(X_unsw, y_unsw, y_unsw.shape[0], verbose=0)
+        unsw_loss.append(score[0])
+        score = m2.evaluate(X_nsl, y_nsl, y_nsl.shape[0], verbose=0)
+        nsl_loss.append(score[0])
 
-united = 640
-num_epochs = 10
-batch_size = 80
-beta = 0.00
+        m2.fit(X_nsl, y_nsl, batch_size, epochs=1, callbacks=[csv_logger])
+        score = m1.evaluate(X_unsw, y_unsw, y_unsw.shape[0], verbose=0)
+        unsw_loss.append(score[0])
+        score = m2.evaluate(X_nsl, y_nsl, y_nsl.shape[0], verbose=0)
+        nsl_loss.append(score[0])
 
-unsw_tens, unsw_inputs, unsw_train, unsw_test, unsw_train_y, unsw_test_y = get_unsw_data()
-nsl_tens, nsl_inputs, nsl_train, nsl_test, nsl_train_y, nsl_test_y = get_nsl_data()
+    shared['unsw_loss'].append(unsw_loss)
+    shared['nsl_loss'].append(nsl_loss)
 
-m1, m2 = master_model(unsw_tens, nsl_tens, unsw_inputs, nsl_inputs)
-
-csv_logger = CSVLogger('ModalityNets/history.log', append=True)
-
-score = m1.evaluate(unsw_train, unsw_train_y, unsw_train_y.shape[0], verbose=1)
-logger.info('UNSW test loss %.6f' % score[0])
-logger.info('UNSW test accu %.6f' % score[1])
-
-for x in range(10):
-    history = m2.fit(nsl_train, nsl_train_y,
-                        epochs=1, batch_size=batch_size, shuffle=True,
-                        steps_per_epoch=None, callbacks=[csv_logger],
-                        validation_data=(nsl_test, nsl_test_y))
-
-    score = m1.evaluate(unsw_train, unsw_train_y, unsw_train_y.shape[0], verbose=1)
-    logger.info('UNSW test loss %.6f' % score[0])
+    score = m1.evaluate(X_unsw, y_unsw, y_unsw.shape[0], verbose=0)
+    logger.debug('UNSW train loss %.6f' % score[0])
+    logger.info('UNSW train accu %.6f' % score[1])
+    shared['unsw']['train'].append(score[1])
+    score = m1.evaluate(X_unsw_test, y_unsw_test, y_unsw_test.shape[0],
+                        verbose=0)
+    logger.debug('UNSW test loss %.6f' % score[0])
     logger.info('UNSW test accu %.6f' % score[1])
+    shared['unsw']['test'].append(score[1])
 
-    '''history = m1.fit(unsw_test, unsw_test_y,
-                     epochs=1, batch_size=batch_size, shuffle=True,
-                     steps_per_epoch=None, callbacks=[csv_logger],
-                     validation_data=(unsw_train, unsw_train_y))'''
+    score = m2.evaluate(X_nsl, y_nsl, y_nsl.shape[0], verbose=0)
+    logger.debug('NSL train loss %.6f' % score[0])
+    logger.info('NSL train accu %.6f' % score[1])
+    shared['nsl']['train'].append(score[1])
+    score = m2.evaluate(X_nsl_test, y_nsl_test, y_nsl_test.shape[0], verbose=0)
+    logger.debug('NSL test loss %.6f' % score[0])
+    logger.info('NSL test accu %.6f' % score[1])
+    shared['nsl']['test'].append(score[1])
 
-score = m2.evaluate(nsl_test, nsl_test_y, nsl_test_y.shape[0], verbose=1)
-logger.info('NSL test loss %.6f' % score[0])
-logger.info('NSL test accu %.6f' % score[1])
 
-score = m1.evaluate(unsw_train, unsw_train_y, unsw_train_y.shape[0], verbose=1)
-logger.info('UNSW test loss %.6f' % score[0])
-logger.info('UNSW test accu %.6f' % score[1])
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
+    root = 'SharedLayer/'
+    logger = logging.getLogger('SharedLayer')
+    hdlr = logging.FileHandler(root + 'accuracy.log')
+    formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
+    hdlr.setFormatter(formatter)
+    logger.addHandler(hdlr)
+    logger.setLevel(logging.INFO)
+
+    unsw_tens, unsw_inputs, X_unsw, X_unsw_test, y_unsw, y_unsw_test = \
+        get_unsw_data()
+    nsl_tens, nsl_inputs, X_nsl, X_nsl_test, y_nsl, y_nsl_test = \
+        get_nsl_data()
+    num_runs = 10
+    hidden_configs = [[256, 480, 400]]
+    for hidden in hidden_configs:
+        shared = {'unsw': {'train': [], 'test': []},
+                  'unsw_loss': [], 'nsl_loss': [],
+                  'nsl': {'train': [], 'test': []}}
+        logger.info('************************************************')
+        logger.info('****  Start %d runs with shared config %s  ****'
+                    % (num_runs, hidden_configs))
+        logger.info('************************************************')
+        for _ in range(num_runs):
+            run_main(hidden)
+        pprint(shared)
+        output = open(root + 'result_runs%s_U%d' % (num_runs, hidden[1]), 'wb+')
+        pickle.dump(shared, output)
+        output.close()
