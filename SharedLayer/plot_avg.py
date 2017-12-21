a367929a2d262df54928efd38ead87229938765e
littlepretty@XcodeMac <littlepretty881203@gmail.com>
2017-12-20 21:04:42 -0600

    shared layer data unpack script
    Signed-off-by: littlepretty@XcodeMac <littlepretty881203@gmail.com>

--- SharedLayer/plot_avg.py
+++ SharedLayer/plot_avg.py
@@ -1,4 +1,21 @@
 import numpy as np
 import pickle
+from pprint import pprint
 
+num_runs = 10
+h = 480
+f = open('result_runs%d_U%d.pkl' % (num_runs, h), 'rb')
+data = pickle.load(f)
 
+shared_unsw_train = data['unsw']['train']
+shared_nsl_train = data['nsl']['train']
+shared_unsw_test = data['unsw']['test']
+shared_nsl_test = data['nsl']['test']
+
+train_avgs = [np.mean(shared_unsw_train), np.mean(shared_nsl_train)]
+train_stds = [np.std(shared_unsw_train), np.std(shared_nsl_train)]
+test_avgs = [np.mean(shared_unsw_test), np.mean(shared_nsl_test)]
+test_stds = [np.std(shared_unsw_test), np.std(shared_nsl_test)]
+pprint(train_avgs)
+pprint(test_avgs)
+pprint(test_stds)
