fcfdc6788d700bb3beca20bffc9753d2197cc950
Jiaqi.Naruto@IITCPUServer <littlepretty881203@gmail.com>
2017-12-28 14:07:13 -0600

    fine/corase train at the same time
    Signed-off-by: Jiaqi.Naruto@IITCPUServer <littlepretty881203@gmail.com>

--- shared_layer.py
+++ shared_layer.py
@@ -2,7 +2,7 @@ from keras.models import Model
 from keras.layers import Dense, Input, concatenate, Flatten, Dropout
 # from keras import regularizers
 from keras.layers import Embedding, BatchNormalization
-from keras.callbacks import CSVLogger
+# from keras.callbacks import CSVLogger
 
 from preprocess import unsw, nslkdd
 from sklearn.preprocessing import LabelEncoder, MinMaxScaler
@@ -182,7 +182,7 @@ def get_nsl_data():
     return merge, merged_inputs, train_dict, test_dict, y, test_y
 
 
-def shared_models(unsw, nsl, unsw_inputs, nsl_inputs, hidden, dropprob=0.2):
+def shared_models(unsw, nsl, unsw_inputs, nsl_inputs, hidden):
     h1_unsw = Dense(hidden[0], activation='relu', name='h1_unsw')(unsw)
     h1_nsl = Dense(hidden[0], activation='relu', name='h1_nsl')(nsl)
     h1_unsw = Dropout(dropprob)(h1_unsw)
@@ -217,22 +217,33 @@ def shared_models(unsw, nsl, unsw_inputs, nsl_inputs, hidden, dropprob=0.2):
 
 
 def run_main(hidden):
-    num_epochs = 20
-    batch_size = 100
     m1, m2 = shared_models(unsw_tens, nsl_tens, unsw_inputs, nsl_inputs, hidden)
-    csv_logger = CSVLogger(root + 'history.log', append=True)
     unsw_loss, nsl_loss = [], []
     for _ in range(num_epochs):
-        m1.fit(X_unsw, y_unsw, batch_size, epochs=1, callbacks=[csv_logger])
-        score = m1.evaluate(X_unsw, y_unsw, y_unsw.shape[0], verbose=0)
-        unsw_loss.append(score[0])
-        score = m2.evaluate(X_nsl, y_nsl, y_nsl.shape[0], verbose=0)
-        nsl_loss.append(score[0])
+        num_batch_runs = -(-max(unsw_size, nsl_size) // batch_size)
+        s1, s2 = 0, 0
+        for _ in range(num_batch_runs):
+            e1 = min(unsw_size, s1 + batch_size)
+            batch_dict = dict()
+            for (key, value) in X_unsw.items():
+                batch_dict[key] = value[s1:e1]
 
-        m2.fit(X_nsl, y_nsl, batch_size, epochs=1, callbacks=[csv_logger])
-        score = m1.evaluate(X_unsw, y_unsw, y_unsw.shape[0], verbose=0)
+            m1.fit(batch_dict, y_unsw[s1:e1, :], batch_size, 1, verbose=0)
+            s1 = 0 if e1 == unsw_size else s1 + batch_size
+
+            e2 = min(nsl_size, s2 + batch_size)
+            batch_dict = dict()
+            for (key, value) in X_nsl.items():
+                batch_dict[key] = value[s2:e2]
+
+            m2.fit(batch_dict, y_nsl[s2:e2, :], batch_size, 1, verbose=0)
+            s2 = 0 if e2 == nsl_size else s2 + batch_size
+
+        m1.fit(X_unsw, y_unsw, batch_size, 1, verbose=0)
+        m2.fit(X_nsl, y_nsl, batch_size, 1, verbose=0)
+        score = m1.evaluate(X_unsw, y_unsw, unsw_size, verbose=0)
         unsw_loss.append(score[0])
-        score = m2.evaluate(X_nsl, y_nsl, y_nsl.shape[0], verbose=0)
+        score = m2.evaluate(X_nsl, y_nsl, nsl_size, verbose=0)
         nsl_loss.append(score[0])
 
     shared['unsw_loss'].append(unsw_loss)
@@ -252,7 +263,8 @@ def run_main(hidden):
     logger.debug('NSL train loss %.6f' % score[0])
     logger.info('NSL train accu %.6f' % score[1])
     shared['nsl']['train'].append(score[1])
-    score = m2.evaluate(X_nsl_test, y_nsl_test, y_nsl_test.shape[0], verbose=0)
+    score = m2.evaluate(X_nsl_test, y_nsl_test, y_nsl_test.shape[0],
+                        verbose=0)
     logger.debug('NSL test loss %.6f' % score[0])
     logger.info('NSL test accu %.6f' % score[1])
     shared['nsl']['test'].append(score[1])
@@ -272,19 +284,27 @@ if __name__ == '__main__':
         get_unsw_data()
     nsl_tens, nsl_inputs, X_nsl, X_nsl_test, y_nsl, y_nsl_test = \
         get_nsl_data()
+    unsw_size, nsl_size = y_unsw.shape[0], y_nsl.shape[0]
     num_runs = 10
-    hidden_configs = [[256, 480, 400]]
+    num_epochs = 42
+    batch_size = 160
+    dropprob = 0.2
+    hidden_configs = [[640, 320, 400]]
     for hidden in hidden_configs:
         shared = {'unsw': {'train': [], 'test': []},
                   'unsw_loss': [], 'nsl_loss': [],
-                  'nsl': {'train': [], 'test': []}}
+                  'nsl': {'train': [], 'test': []},
+                  'epochs': num_epochs, 'batch_size': batch_size,
+                  'dropprob': dropprob, 'hidden': hidden}
         logger.info('************************************************')
         logger.info('****  Start %d runs with shared config %s  ****'
-                    % (num_runs, hidden_configs))
+                    % (num_runs, hidden))
         logger.info('************************************************')
         for _ in range(num_runs):
             run_main(hidden)
-        pprint(shared)
-        output = open(root + 'result_runs%s_U%d' % (num_runs, hidden[1]), 'wb+')
+        pprint(shared['unsw'])
+        pprint(shared['nsl'])
+        output = open(root + 'result_runs%s_U%d.pkl' % (num_runs, hidden[1]),
+                      'wb+')
         pickle.dump(shared, output)
         output.close()
