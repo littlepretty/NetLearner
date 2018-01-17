21d8771790077520c4ba30efb905cb6cb135e70b
Jiaqi@Nvidia1080Ti4 <littlepretty881203@gmail.com>
2018-01-17 08:02:10 -0800

    incremental training + plot figures
    Signed-off-by: Jiaqi@Nvidia1080Ti4 <littlepretty881203@gmail.com>

--- wd_nslkdd.py
+++ wd_nslkdd.py
@@ -5,9 +5,10 @@ from __future__ import print_function
 import pandas as pd
 import tensorflow as tf
 import numpy as np
+import matplotlib.pyplot as plt
 import pickle
 import logging
-import matplotlib.pyplot as plt
+import os
 from sklearn.preprocessing import MinMaxScaler, QuantileTransformer
 from preprocess.nslkdd import get_feature_names, get_categorical_values
 
@@ -48,6 +49,7 @@ def augment_dataset(fname, output_path):
         print('Scaler already fitted')
         numeric = scaler.transform(numeric)
     else:
+        print('First time fit scaler')
         scaler.fit(numeric)
         numeric = scaler.transform(numeric)
         scaler_fitted = True
@@ -56,6 +58,7 @@ def augment_dataset(fname, output_path):
         print('Transformer already fitted')
         augment = transformer.transform(numeric)
     else:
+        print('First time fit transformer')
         transformer.fit(numeric)
         augment = transformer.transform(numeric)
         transformer_fitted = True
@@ -90,29 +93,25 @@ def train_and_eval(model_dir, mtype, columns, train_filename, test_filename):
     m = build_model(model_dir, mtype)
     train_ib = input_builder(train_filename, columns)
     test_ib = input_builder(test_filename, columns)
-    history = []
+    history = {'train': [], 'test': []}
     for i in range(num_epochs):
         m.train(input_fn=train_ib)
         results = m.evaluate(train_ib)
-        history.append(results)
+        history['train'].append(results)
         logger.info('******   Train performance   ******')
         for key in results:
             logger.info("%s: %s" % (key, results[key]))
 
         results = m.evaluate(input_fn=test_ib)
+        history['test'].append(results)
         logger.info('******   Test performance   ******')
         for key in results:
             logger.info("%s: %s" % (key, results[key]))
 
-    results = m.evaluate(input_fn=test_ib)
-    history.append(results)
-    logger.info('Test performance')
-    for key in results:
-        logger.info("%s: %s" % (key, results[key]))
-
     return history
 
 
+os.environ['CUDA_VISIBLE_DEVICES'] = '0'
 CSV_COLUMNS, symbolic_names, continuous_names, discrete_names = \
     get_feature_names('NSLKDD/feature_names.csv')
 header = ""
@@ -188,7 +187,7 @@ test_filename = 'NSLKDD/KDDTest.csv'
 model_dir = 'WideDeepModel/NSLKDD/'
 train_path = model_dir + 'aug_train.csv'
 test_path = model_dir + 'aug_test.csv'
-num_epochs = 48
+num_epochs = 160
 batch_size = 160
 dropout = 0.2
 
@@ -208,15 +207,41 @@ logger.setLevel(logging.INFO)
 columns = augment_dataset(train_filename, train_path)
 augment_dataset(test_filename, model_dir + 'aug_test.csv')
 hist = train_and_eval(model_dir, 'WnD', columns, train_path, test_path)
-output = open(model_dir + 'Runs%d.pkl' % (num_epochs), 'wb+')
+output = open(model_dir + 'Runs%d.pkl' % (num_epochs), 'wb')
 pickle.dump(hist, output)
 output.close()
-plt.figure(0)
-plt.plot([x['accuracy'] for x in hist], 'r--')
+
+"""
+model_dir = 'WideDeepModel/NSLKDD/'
+num_epochs = 320
+output = open(model_dir + 'Runs%d.pkl' % (num_epochs), 'rb')
+hist = pickle.load(output)
+"""
+fig, ax1 = plt.subplots()
+ax1.plot([x['accuracy'] for x in hist['train']], 'r--')
+ax1.set_ylabel('train', color='r')
+ax1.tick_params('y', colors='r')
+ax2 = ax1.twinx()
+ax2.plot([x['accuracy'] for x in hist['test']], 'b:')
+ax2.set_ylabel('test', color='b')
+ax2.tick_params('y', colors='b')
+ax1.grid(color='k', linestyle=':', linewidth=1)
+ax2.grid(color='k', linestyle=':', linewidth=1)
+fig.tight_layout()
 plt.savefig(model_dir + 'accu_%d.pdf' % num_epochs, format='pdf')
 plt.close()
 
-plt.figure(1)
-plt.plot([x['loss'] for x in hist], 'b--')
+fig, ax1 = plt.subplots()
+ax1.plot([x['loss'] for x in hist['train']], 'r--')
+ax1.set_ylabel('train', color='r')
+ax1.tick_params('y', colors='r')
+ax2 = ax1.twinx()
+ax2.plot([x['loss'] for x in hist['test']], 'b:')
+ax2.set_ylabel('test', color='b')
+ax2.tick_params('y', colors='b')
+ax1.grid(color='k', linestyle=':', linewidth=1)
+ax2.grid(color='k', linestyle=':', linewidth=1)
+fig.tight_layout()
+plt.grid(which='both', color='k', linestyle=':', linewidth=1)
 plt.savefig(model_dir + 'loss_%d.pdf' % num_epochs, format='pdf')
 plt.close()
